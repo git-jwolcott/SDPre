3890fcfdc9662080ea8c0331a04d5b344ff3e3a4
jwolcott <jeanne.wolcott@gmail.com>
2020-08-19 14:24:21 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Jeanne Wolcott";
+    int age = 51;
+    String state = "Texas";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
